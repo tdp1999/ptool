028516d91d0bb93f932db0fa70cfe9a52c98ced9
tdp99 <tdp99.business@gmail.com>
2024-06-12 15:33:22 +0700

    feat: add tailwind forms and angular cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+} from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, LayoutModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ptool';
+  subscription: Subscription | undefined;
+  breakpointObserver = inject(BreakpointObserver);
+
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
+
+  ngOnInit(): void {
+    this.subscription = this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .subscribe((result) => {
+        for (const query of Object.keys(result.breakpoints)) {
+          console.log('Query', query, 'Matches', result.breakpoints[query]);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
